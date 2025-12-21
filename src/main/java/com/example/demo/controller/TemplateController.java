18ed86ec90a6d95cb44c242f95081b656a3a9c2c
bala-amypo <bala@amypo.in>
2025-12-21 07:47:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/TemplateController.java
+++ src/main/java/com/example/demo/controller/TemplateController.java
@@ -10,19 +10,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/templates")
 public class TemplateController {
-
+    
     private final TemplateService templateService;
-
+    
     public TemplateController(TemplateService templateService) {
         this.templateService = templateService;
     }
-
+    
     @PostMapping
     public ResponseEntity<CertificateTemplate> addTemplate(@RequestBody CertificateTemplate template) {
         CertificateTemplate savedTemplate = templateService.addTemplate(template);
         return ResponseEntity.ok(savedTemplate);
     }
-
+    
     @GetMapping
     public ResponseEntity<List<CertificateTemplate>> getAllTemplates() {
         List<CertificateTemplate> templates = templateService.getAllTemplates();
