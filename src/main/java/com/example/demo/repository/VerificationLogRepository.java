928a6d24dc823b7d9cf764d1c69b5afaf217c1c2
bala-amypo <bala@amypo.in>
2025-12-21 16:11:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/VerificationLogRepository.java
+++ src/main/java/com/example/demo/repository/VerificationLogRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.VerificationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.demo.entity.VerificationLog;
+import com.example.demo.entity.Certificate;
+import java.util.List;
 
 @Repository
 public interface VerificationLogRepository extends JpaRepository<VerificationLog, Long> {
+
+    List<VerificationLog> findByCertificate(Certificate certificate);
 }
